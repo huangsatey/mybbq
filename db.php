1e04b195350666784655ca1ffbbf65db85a017f5
huangsatey <61459273+huangsatey@users.noreply.github.com>
2020-05-23 21:17:02 +0800

    Update db.php

--- db.php
+++ db.php
@@ -4,10 +4,5 @@ $username = "root";
 $password = "root";
 $dbname = "mybbq";
 $connID = mysqli_connect($servername, $username, $password, $dbname);
-/*
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-*/
-$background = "img/background.jpg";
+$background = "img/background.jpg"; //background photo
 ?>
